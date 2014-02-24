16a644779ec85581b26f198b2b8b48e141293ecd
Alexis Sukrieh <sukria@sukria.net>
2014-02-24 12:01:05 +0100

    first working version

--- lib/Ustone.pm
+++ lib/Ustone.pm
@@ -1,14 +1,31 @@
 package Ustone;
 use Dancer2;
 
+use feature ':5.10';
 our $VERSION = '0.1';
+use Ustone::DB;
+
+sub db {
+    state $db = Ustone::DB->new( 
+        db_path => config->{'application'}->{'db_path'} 
+    );
+}
 
 get '/' => sub {
-    template 'index';
+    template 'index', { 
+        top => db->fetch_last_top,
+        current => db->fetch_last_issue,
+    };
 };
 
+get '/new' => sub { template 'new' };
+
 post '/new' => sub {
-    
+    db->create(
+        description => param('description'), 
+        root_cause => param('root_cause'),
+    );
+    redirect '/';
 };
 
 true;
